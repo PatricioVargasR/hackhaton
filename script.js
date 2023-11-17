baa32a9c7101955d867c8421fd947e5977b95e01
Paulinatello <78061704+Pauu1@users.noreply.github.com>
2023-11-17 03:49:04 -0600

    Perfil de usuario

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Puedes cargar datos del usuario desde el backend aquí y actualizar el formulario si es necesario
+});
+
+document.getElementById("profileForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Puedes agregar lógica aquí para enviar el formulario al servidor
+    // y manejar la actualización de datos del usuario
+
+    alert("¡Cambios guardados!");
+});
